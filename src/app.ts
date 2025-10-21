b65d58a84775c6a2812f2304f895d3884202aea4
tomasbennett <tjs.bennett@gmail.com>
2025-10-21 10:59:13 +0100

    Adding in the styling public files for the main home page and fixing an error whereby static files pointed to the wrong folder as I didn't point to __dirname first

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(expressLayouts);
 app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("layout", "layouts/mainLayout");
 
